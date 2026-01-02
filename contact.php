91516f41ccd3a29572ca47c9da26a6d121972872
prachijhunjhunwala <prachijhunjhunwala204@gmail.com>
2026-01-02 22:24:07 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+<main class="content">
+  <h1>Contact</h1>
+  <p>Email: support@bcclclone.com</p>
+  <form>
+    <input type="text" placeholder="Your Name"><br><br>
+    <input type="email" placeholder="Your Email"><br><br>
+    <textarea placeholder="Your Message"></textarea><br><br>
+    <button type="submit">Send</button>
+  </form>
+</main>
+
+<?php include 'footer.php'; ?>
