33dfed2eebc33dbdcddf601d30a0eb9b180edcfd
prachijhunjhunwala <prachijhunjhunwala204@gmail.com>
2026-01-02 21:06:28 +0530

    Create services.php

---
+++ services.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+
+<main class="content">
+  <h1>Services</h1>
+  <ul>
+    <li>AI Solutions</li>
+    <li>Web Development</li>
+    <li>Finance Analytics</li>
+  </ul>
+</main>
+
+<?php include 'footer.php'; ?>
